af49f6b5f27786345493622911f2bb4561f9bc86
darthsyn <97857506+darthsyn@users.noreply.github.com>
2023-11-13 17:20:44 +0000

    explored jsonify api and a different way to pull data to the page

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id':1,
+    'title': 'Python Developer',
+    'location': 'New York',
+    'salary': '$100,000'
+  },
+  {
+    'id':2,
+    'title': 'Cool Dude',
+    'location': 'New York',
+    'salary': '$100,000,000'
+  },
+{
+  'id':3,
+  'title': 'Music Developer',
+  'location': 'Remote',  
+}
+]
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', 
+                         jobs=JOBS,
+                        company_name='Tactical Treehouse')
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
